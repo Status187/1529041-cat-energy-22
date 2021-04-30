48c5c09251cc22b1af120bdf94e0a71e5c62c6d0
Maksim <status187@yandex.ru>
2021-04-30 14:27:10 +0300

    Картинки для Index

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,4 @@
-let addBurger = document.querySelector('.menu__burger');
+/*let addBurger = document.querySelector('.menu__burger');
 
 let addBurgerAll = document.querySelector('.menu__burger--closed');
 
@@ -7,3 +7,20 @@ addBurger.addEventListener('click', event => {
   addBurger.classList.toggle('closed');
 addBurgerAll.classList.toggle('closed');
 })
+
+
+var navMain = document.querySelector('.main-nav');
+  var navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+*/
